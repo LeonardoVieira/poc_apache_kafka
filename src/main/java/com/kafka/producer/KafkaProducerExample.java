2ebf1671d11f1604cc8311bf018cdd46d18533fe
leonardo <leonardo.vieira2612@gmail.com>
2015-02-24 13:32:33 -0300

    initial commit

---
+++ src/main/java/com/kafka/producer/KafkaProducerExample.java
@@ -0,0 +1,38 @@
+package com.kafka.producer;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class KafkaProducerExample {
+
+	public static void main(final String[] args) throws IOException {
+		long events = Long.parseLong("5");
+        Random rnd = new Random();
+        Properties props = new Properties();
+
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "com.kafka.SimplePartitioner");
+        props.put("request.required.acks", "1");
+ 
+        ProducerConfig config = new ProducerConfig(props);
+ 
+        Producer<String, String> producer = new Producer<String, String>(config);
+ 
+        for (long nEvents = 0; nEvents < events; nEvents++) { 
+               long runtime = new Date().getTime();  
+               String ip = "192.168.2." + rnd.nextInt(255); 
+               String msg = runtime + ",www.example.com," + ip;
+               KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", ip, msg);
+               producer.send(data);
+        }
+
+        producer.close();
+	}
+}
